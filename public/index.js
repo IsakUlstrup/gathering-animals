3c3df2ad45d7a18b384f8fa056e5d9e1dde15fde
Isak <isakulstrup@gmail.com>
2022-09-26 08:29:30 +0200

    implemented storeInventory port

--- public/index.js
+++ public/index.js
@@ -1,6 +1,18 @@
 import { Elm } from '../src/Main.elm';
 
-Elm.Main.init({
-  node: document.querySelector('main')
+const storedState = localStorage.getItem('inventory');
+console.log("Retrieved state: ", storedState);
+const startingState = storedState ? JSON.parse(storedState) : null;
+
+const app = Elm.Main.init({
+  node: document.querySelector('main'),
+  flags: startingState
 });
 
+app.ports.storeInventory.subscribe(function (items) {
+  if (items.length > 0) {
+    const itemsJson = JSON.stringify(items);
+    localStorage.setItem('inventory', itemsJson);
+    console.log("Saved state: ", itemsJson);
+  }
+});
