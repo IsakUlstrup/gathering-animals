7a6e072b9024d69622f8270bffbe52a7aa11f6ed
Isak <isakulstrup@gmail.com>
2022-09-27 09:28:17 +0200

    added comments to functions prevented negative remaining state time

--- src/Engine/Animal.elm
+++ src/Engine/Animal.elm
@@ -12,42 +12,58 @@ module Engine.Animal exposing
 import Engine.Resource as Resource exposing (Resource)
 
 
+{-| Animal state, the integers represent remaining time in each state
+-}
 type AnimalState
     = Idle
     | Interact Int
     | Cooldown Int
 
 
+{-| Idle state constructor
+-}
 idleState : AnimalState
 idleState =
     Idle
 
 
+{-| Interact state constructor
+-}
 interactState : AnimalState
 interactState =
     Interact 200
 
 
+{-| Cooldown state constructor
+-}
 cooldownState : AnimalState
 cooldownState =
     Cooldown 1000
 
 
+{-| Animal type, just the state for now
+-}
 type alias Animal =
     { state : AnimalState
     }
 
 
+{-| Animal constructor
+-}
 new : Animal
 new =
     Animal cooldownState
 
 
+{-| Set animal state to idle
+-}
 setIdle : Animal -> Animal
 setIdle animal =
     { animal | state = idleState }
 
 
+{-| Is animal idle predicate
+-}
 isIdle : Animal -> Bool
 isIdle animal =
     case animal.state of
@@ -58,11 +74,15 @@ isIdle animal =
             False
 
 
+{-| Set animal state to interact
+-}
 setInteract : Animal -> Animal
 setInteract animal =
     { animal | state = interactState }
 
 
+{-| Is animal interacting predicate
+-}
 isInteracting : Animal -> Bool
 isInteracting animal =
     case animal.state of
@@ -73,11 +93,15 @@ isInteracting animal =
             False
 
 
+{-| Set animal state to cooldown
+-}
 setCooldown : Animal -> Animal
 setCooldown animal =
     { animal | state = cooldownState }
 
 
+{-| Is animal cooling predicate
+-}
 isCooling : Animal -> Bool
 isCooling animal =
     case animal.state of
@@ -88,17 +112,24 @@ isCooling animal =
             False
 
 
+{-| Reduce remaining time for each state, does not advance state
+-}
 tickState : Int -> Animal -> Animal
 tickState dt animal =
+    let
+        reduceTime : Int -> Int -> Int
+        reduceTime t d =
+            t - d |> max 0
+    in
     case animal.state of
         Idle ->
             animal
 
         Interact time ->
-            { animal | state = Interact <| time - dt }
+            { animal | state = Interact <| reduceTime time dt }
 
         Cooldown time ->
-            { animal | state = Cooldown <| time - dt }
+            { animal | state = Cooldown <| reduceTime time dt }
 
 
 {-| Advance to next state if timer is done, otherwise reduce current state timer
